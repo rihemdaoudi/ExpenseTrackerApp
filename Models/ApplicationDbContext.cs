eb86e35be928acf8b23a5f810ff4e46c9b3d0480
rihemdaoudi <rihemdaoudi42@gmail.com>
2023-05-26 01:05:21 +0200

    Expense Tracker App

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Models
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions options):base(options) 
+		{
+		}
+
+		public DbSet<Transaction> Transaction { get; set; }
+		public DbSet<Category> Category { get; set; }
+	}
+}
